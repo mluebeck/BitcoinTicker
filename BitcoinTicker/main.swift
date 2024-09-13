037124daa221cfd0dbf4450576605aec7bbfe71c
mluebeck <mariorotz@googlemail.com>
2024-09-13 19:11:27 +0200

    first commit

---
+++ BitcoinTicker/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  BitcoinTicker
+//
+//  Created by Mario Rotz on 12.09.24.
+//
+
+import Foundation
+ 
---
+++ BitcoinTickerTest/BitcoinTickerTest.swift
@@ -0,0 +1,25 @@
+//
+//  BitcoinTickerTest.swift
+//  BitcoinTickerTest
+//
+//  Created by Mario Rotz on 12.09.24.
+//
+
+import XCTest
+import BitcoinTicker
+
+
+final class BitcoinTickerTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        
+    }
+}
