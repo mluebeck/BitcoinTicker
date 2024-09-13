99176ef0057fd60914693732627f417c2c4ad3ba
mluebeck <mariorotz@googlemail.com>
2024-09-14 00:08:03 +0200

    refactor tests to remove duplications

--- BitcoinTickerTest/BitcoinTickerTest.swift
+++ BitcoinTickerTest/BitcoinTickerTest.swift
@@ -25,27 +25,30 @@ protocol HTTPClient {
     func get(from url:URL)
 }
 
-class HTTPClientSpy : HTTPClient {
-    func get(from url:URL) {
-        requestedURL = url
-    }
-    var requestedURL : URL?
-}
 
 final class BitcoinTickerTest: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() throws {
-        let url = URL(string:"https://a-url.com")!
-        let client = HTTPClientSpy()
-        _ = BitcoinDataLoader(url:url,client: client)
+        let (_,client) = makeSUT()
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let url = URL(string:"https://a-url.com")!
-        let client = HTTPClientSpy()
-        let sut = BitcoinDataLoader(url:url,client: client)
+        let (sut,client) = makeSUT()
         sut.load()
         XCTAssertNotNil(client.requestedURL)
+    }
+    
+    private func makeSUT(url:URL = URL(string:"https://a-url.com")!) -> (sut:BitcoinDataLoader,client:HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = BitcoinDataLoader(url: url, client: client)
+        return (sut,client)
+    }
+    
+    private class HTTPClientSpy : HTTPClient {
+        var requestedURL : URL?
+        func get(from url:URL) {
+            requestedURL = url
+        }
     }
 }
