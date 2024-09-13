bf4e62edfc847eac8b74ac2f81eba29f2ff46656
mluebeck <mariorotz@googlemail.com>
2024-09-13 23:25:13 +0200

    Move test logic to a separate type HTTPClientSpy

--- BitcoinTickerTest/BitcoinTickerTest.swift
+++ BitcoinTickerTest/BitcoinTickerTest.swift
@@ -15,21 +15,29 @@ class BitcoinDataLoader {
 }
 
 class HTTPClient {
-    static let shared = HTTPClient()
-    private init() {}
+    static var shared = HTTPClient()
     var requestedURL : URL?
+    func get(from url:URL) {}
+}
+
+class HTTPClientSpy : HTTPClient {
+    override func get(from url:URL) {
+        requestedURL = url
+    }
 }
 
 final class BitcoinTickerTest: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() throws {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = BitcoinDataLoader()
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = BitcoinDataLoader()
         sut.load()
         XCTAssertNotNil(client.requestedURL)
