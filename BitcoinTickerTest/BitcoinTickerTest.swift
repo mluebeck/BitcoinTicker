cb9e1b5b6eb5e574c5ff8e82a81b7b7cbabd699d
mluebeck <mariorotz@googlemail.com>
2024-09-17 00:12:56 +0200

    Improve readability with helper method

--- BitcoinTickerTest/BitcoinTickerTest.swift
+++ BitcoinTickerTest/BitcoinTickerTest.swift
@@ -38,7 +38,7 @@ final class BitcoinTickerTest: XCTestCase {
             capturedErrors.append($0)
         }
         let clientError = NSError(domain: "Test", code: 0)
-        client.completions[0](clientError)
+        client.complete(with: clientError)
         XCTAssertEqual(capturedErrors,[.connectivity])
     }
     
@@ -59,6 +59,10 @@ final class BitcoinTickerTest: XCTestCase {
         func get(from url: URL, completion:@escaping (any Error) -> Void) {
                    completions.append(completion)
             requestedURLs.append(url)
+        }
+        
+        func complete(with error:Error, at index:Int=0) {
+            completions[index](error)
         }
     }
 }
